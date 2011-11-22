2c891eb53a6caa010e3ad49b413b67e680039381
Libor Wagner <liborwagner@gmail.com>
2011-11-22 08:59:07 +0100

    English localisation

---
+++ lang_en.m
@@ -0,0 +1,13 @@
+% English localization
+lang = struct()
+lang.instructions = {['You will be shown words, one at a time, printed in different colors.',...
+                      ' Press r for word printed in red, g for green, b for blue and m for',...
+                      ' magenta. Try to be as fast as possible but correct.'], ['[new press space',...
+                      ' to start the experiment]']};
+
+lang.correct = {'correct'};
+lang.incorect = {'incorrect'};
+
+lang.words = {'red', 'green', 'blue', 'magenta'};
+lang.colors = lang.words;
+lang.keys = 'rgbm';
