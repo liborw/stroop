7ffee5f09042b3ada0bb3876e0f54bf468dee3bb
Libor Wagner <liborwagner@gmail.com>
2011-11-22 09:32:28 +0100

    Czech localization

---
+++ lang_cs.m
@@ -0,0 +1,12 @@
+% English localization
+lang = struct();
+lang.instructions = {['V tomto oknì se budou obìvovat barevná slova va¹ím úkolem je',...
+                      ' zmáèkout c pro slovo zobrazené èervenou barvou, z pro',...
+                      'zelené, m pro modré a f pro fialové slovo.'],...
+                     '[niní zmáèkni mezerník pro spu¹tìní experimentu]'};
+lang.correct = {'správnì'};
+lang.incorect = {'¹patnì'};
+
+lang.words = {'èervená', 'zelená', 'modrá', 'fialová'};
+lang.colors = {'red', 'green', 'blue', 'magenta'};
+lang.keys = 'czmf';
